a8601857ec54cd815ec8d3dd51320f6da20827fa
Philip Craig <philipjcraig@gmail.com>
2016-07-17 18:02:16 +1000

    Inline low_bits_of_byte and low_bits_of_u64
    Before:
    test read_signed    ... bench:       9,043 ns/iter (+/- 488)
    test read_unsigned  ... bench:       8,987 ns/iter (+/- 395)
    test write_signed   ... bench:      19,667 ns/iter (+/- 914)
    test write_unsigned ... bench:      17,965 ns/iter (+/- 751)
    
    After:
    test read_signed    ... bench:       6,656 ns/iter (+/- 409)
    test read_unsigned  ... bench:       5,889 ns/iter (+/- 182)
    test write_signed   ... bench:      19,589 ns/iter (+/- 985)
    test write_unsigned ... bench:      14,849 ns/iter (+/- 798)

--- src/lib.rs
+++ src/lib.rs
@@ -48,11 +48,13 @@ pub const CONTINUATION_BIT: u8 = 1 << 7;
 pub const SIGN_BIT: u8 = 1 << 6;
 
 #[doc(hidden)]
+#[inline]
 pub fn low_bits_of_byte(byte: u8) -> u8 {
     byte & !CONTINUATION_BIT
 }
 
 #[doc(hidden)]
+#[inline]
 pub fn low_bits_of_u64(val: u64) -> u8 {
     let byte = val & (std::u8::MAX as u64);
     low_bits_of_byte(byte as u8)
